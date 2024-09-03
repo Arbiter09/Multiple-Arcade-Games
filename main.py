90f044941715749e10d1cd3a0eb169b74bb61fb2
Arbiter09 <100960442+Arbiter09@users.noreply.github.com>
2024-09-03 15:47:49 -0400

    Updated File structure

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame, sys
 from button import Button
 import Pong
 import Naruto_Game
-
+import Snake
 
 pygame.init()
 
@@ -17,9 +17,10 @@ def get_font(size):
 def play():
    Pong.game()
         
-def options():
+def naruto():
     Naruto_Game.Naruto()
-
+def Snnake():
+    Snake.snake()
 def main_menu():
     while True:
         SCREEN.blit(BG, (0, 0))
@@ -31,14 +32,14 @@ def main_menu():
 
         PLAY_BUTTON = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(640, 250), 
                             text_input="PONG", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
-        OPTIONS_BUTTON = Button(image=pygame.image.load("assets/Options Rect.png"), pos=(640, 400), 
+        Naruto_Button = Button(image=pygame.image.load("assets/Options Rect.png"), pos=(640, 400), 
                             text_input="Naruto", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
-        QUIT_BUTTON = Button(image=pygame.image.load("assets/Quit Rect.png"), pos=(640, 550), 
-                            text_input="QUIT", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+        Snake_button = Button(image=pygame.image.load("assets/Quit Rect.png"), pos=(640, 550), 
+                            text_input="Snake", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
 
         SCREEN.blit(MENU_TEXT, MENU_RECT)
 
-        for button in [PLAY_BUTTON, OPTIONS_BUTTON, QUIT_BUTTON]:
+        for button in [PLAY_BUTTON, Naruto_Button, Snake_button]:
             button.changeColor(MENU_MOUSE_POS)
             button.update(SCREEN)
         
@@ -49,11 +50,10 @@ def main_menu():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if PLAY_BUTTON.checkForInput(MENU_MOUSE_POS):
                     play()
-                if OPTIONS_BUTTON.checkForInput(MENU_MOUSE_POS):
-                    options()
-                if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
-                    pygame.quit()
-                    sys.exit()
+                if Naruto_Button.checkForInput(MENU_MOUSE_POS):
+                    naruto()
+                if Snake_button.checkForInput(MENU_MOUSE_POS):
+                    Snnake()
 
         pygame.display.update()
 
